74e97db90a0b2c4c116aa77de5ca335e93fe5379
mremtf <matt.r.england@gmail.com>
2015-07-01 20:38:50 -0500

    adding base files and folders

---
+++ include/c_refresher.h
@@ -0,0 +1,4 @@
+#ifndef _C_REFRESHER_
+#define _C_REFRESHER_
+
+#endif
---
+++ src/runner.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+
+
+#include "../include/c_refresher.h"
+
+
+int main (int argc, char **argv) {
+	
+	
+}
