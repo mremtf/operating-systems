776aaf0fbcebb100e6b04e05073b9f1de630850f
Will Starms <vilhelmen@gmail.com>
2015-07-15 16:10:09 -0500

    bitmap finished. Gotta mess with the cmake for libs and autotesting and whatnot

--- algorithms/bitmap/include/bitmap.h
+++ algorithms/bitmap/include/bitmap.h
@@ -5,42 +5,48 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <string.h>
 
 typedef struct bitmap bitmap_t;
 
+// WARNING: Bit requests outside the bitmap and NULL pointers WILL result in a segfault
+// This is by design. There is as little overhead as possible. Use it right and everything will be fine.
+
 // Sets requested bit in bitmap
-void bitmap_set(bitmap_t* bitmap, size_t bit);
+void bitmap_set(bitmap_t *bitmap, size_t bit);
 
 // Clears requested bit in bitmap
-void bitmap_unset(bitmap_t* bitmap, size_t bit);
+void bitmap_reset(bitmap_t *bitmap, size_t bit);
 
 // Returns bit in bitmap
-bool bitmap_test(bitmap_t* bitmap, size_t bit);
-bool bitmap_at(bitmap_t* bitmap, size_t bit);
+bool bitmap_test(bitmap_t *bitmap, size_t bit);
+bool bitmap_at(bitmap_t *bitmap, size_t bit);
 
 // Flips bit in bitmap
-void bitmap_flip(bitmap_t* bitmap, size_t bit);
+void bitmap_flip(bitmap_t *bitmap, size_t bit);
 
 // Resets bitmap contents to the desired pattern
-void bitmap_reset(bitmap_t* bitmap, uint8_t pattern);
+// (pattern not guarenteed accurate for final bits
+// 		if bit_count is not a multiple of 8)
+void bitmap_format(bitmap_t *bitmap, uint8_t pattern);
 
 // Gets total number of bits in bitmap
-size_t bitmap_get_bits(bitmap_t* bitmap);
+size_t bitmap_get_bits(bitmap_t *bitmap);
 
 // Gets total number of bytes in bitmap
-size_t bitmap_get_bytes(bitmap_t* bitmap);
+size_t bitmap_get_bytes(bitmap_t *bitmap);
 
 // Gets pointer for writing bitmap to file
-uint8_t* bitmap_get_data(bitmap_t* bitmap);
+uint8_t *bitmap_get_data(bitmap_t *bitmap);
 
 // Creates a new bitmap with the provided data and size
-bool bitmap_import(bitmap_t* bitmap, size_t n_bits, uint8_t* bitmap_data);
+bitmap_t *bitmap_import(size_t n_bits, uint8_t *bitmap_data);
 
 // Creates a bitmap to contain n bits (zero initialized)
-bool bitmap_initialize(bitmap_t* bitmap, size_t n_bits);
+bitmap_t *bitmap_initialize(size_t n_bits);
 
-// Destructs and bitmap object
-void bitmap_destroy(bitmap_t* bitmap);
+// Destructs and destroys bitmap object
+void bitmap_destroy(bitmap_t *bitmap);
 
 
 #endif
--- algorithms/bitmap/src/bitmap.c
+++ algorithms/bitmap/src/bitmap.c
@@ -1,51 +1,56 @@
 #include "../include/bitmap.h"
 
 struct bitmap {
-    uint8_t* data;
+    uint8_t *data;
     size_t bit_count, byte_count;
-}
+};
 
-void bitmap_set(bitmap_t* bitmap, size_t bit) { bitmap->data[bit >> 3] |= 0x01 << (bit & 0x07); }
+void bitmap_set(bitmap_t *bitmap, size_t bit) { bitmap->data[bit >> 3] |= 0x01 << (bit & 0x07); }
 
-void bitmap_unset(bitmap_t* bitmap, size_t bit) { bitmap->data[bit >> 3] &= ~(0x01 << (bit & 0x07)); }
+void bitmap_reset(bitmap_t *bitmap, size_t bit) { bitmap->data[bit >> 3] &= ~(0x01 << (bit & 0x07)); }
 
-bool bitmap_test(bitmap_t* bitmap, size_t bit) { return bitmap->data[bit >> 3] & (0x01 << (bit & 0x07)); }
-bool bitmap_at(bitmap_t* bitmap, size_t bit) { return bitmap_test(bitmap, bit); }
+bool bitmap_test(bitmap_t *bitmap, size_t bit) { return bitmap->data[bit >> 3] & (0x01 << (bit & 0x07)); }
+bool bitmap_at(bitmap_t *bitmap, size_t bit) { return bitmap_test(bitmap, bit); }
 
-void bitmap_flip(bitmap_t* bitmap, size_t bit) { bitmap->data[bit >> 3] ^= (0x01 << (bit & 0x07)); }
+void bitmap_flip(bitmap_t *bitmap, size_t bit) { bitmap->data[bit >> 3] ^= (0x01 << (bit & 0x07)); }
 
-size_t bitmap_get_bits(bitmap_t* bitmap) { return bitmap->bit_count; }
-size_t bitmap_get_bytes(bitmap_t* bitmap) { return bitmap->byte_count; }
-uint8_t* bitmap_get_data(bitmap_t* bitmap) { return bitmap->data; }
+size_t bitmap_get_bits(bitmap_t *bitmap) { return bitmap->bit_count; }
+size_t bitmap_get_bytes(bitmap_t *bitmap) { return bitmap->byte_count; }
+uint8_t *bitmap_get_data(bitmap_t *bitmap) { return bitmap->data; }
 
-void bitmap_reset(bitmap_t* bitmap, uint8_t pattern) { memset(bitmap->data, pattern, byte_count); }
+void bitmap_format(bitmap_t *bitmap, uint8_t pattern) { memset(bitmap->data, pattern, bitmap->byte_count); }
 
-bool bitmap_initialize(bitmap_t* bitmap, size_t n_bits) {
-    if (bitmap) {
-        if (n_bits) { // must be non-zero
+bitmap_t *bitmap_initialize(size_t n_bits) {
+    if (n_bits) { // must be non-zero
+        bitmap_t *bitmap = (bitmap_t *) malloc(sizeof(bitmap_t));
+        if (bitmap) {
             bitmap->bit_count = n_bits;
             bitmap->byte_count = n_bits >> 3;
             bitmap->byte_count += (n_bits & 0x07 ? 1 : 0);
-            bitmap->data = (uint8_t*)calloc(byte_count,1);
+            bitmap->data = (uint8_t *)calloc(bitmap->byte_count, 1);
 
             if (bitmap->data) {
-                return true;
+                return bitmap;
             }
+            free(bitmap);
         }
     }
-    return false;
+    return NULL;
 }
 
-bool bitmap_import(bitmap_t* bitmap, size_t n_bits, uint8_t* bitmap_data) {
-    if (bitmap_initialize(bitmap, n_bits, 0x00)) {
+bitmap_t *bitmap_import(size_t n_bits, uint8_t *bitmap_data) {
+    bitmap_t *bitmap = bitmap_initialize(n_bits);
+    if (bitmap && bitmap_data) {
         memcpy(bitmap->data, bitmap_data, bitmap->byte_count);
-        return true;
+        return bitmap;
     }
-    return false;
+    free(bitmap);
+    return NULL;
 }
 
-void bitmap_destroy(bitmap_t* bitmap) {
+void bitmap_destroy(bitmap_t *bitmap) {
     if (bitmap) {
         free(bitmap->data);
+        free(bitmap);
     }
 }
